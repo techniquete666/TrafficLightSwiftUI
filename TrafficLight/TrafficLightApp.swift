6dbebf6813f8afb240488b7d3b54cab99a868270
techniquete666 <dodyaozxc@gmail.com>
2022-12-10 11:55:57 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Vyacheslav on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
